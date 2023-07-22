125a2e8b3efd02b8b47063da8ffd0a880ad67747
Ermak102 <60349645+ermakovss@users.noreply.github.com>
2023-07-22 17:04:38 +0300

    add adaptive

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import HomePage from './views/HomePage';
 
 import './styles/index.css';
+import './styles/adaptive.css';
 
 const App = () => {
   return <HomePage />;
